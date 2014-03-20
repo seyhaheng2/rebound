4a56ff0fa8dceefe606e04c016ceefeb44386a57
seyhaheng <seyhaheng2@gmail.com>
2014-03-20 13:04:18 +0700

    done with gem file

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rebound::Application.config.secret_key_base = '7606e3470ee55ca52290e41591d0da082648ae71c4cf210efde3f2bb9d25d59821afa9fa7112cdaa470cf8b5073bae0195c23bab98f1398197dbbeaed44a1ad7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rebound::Application.config.session_store :cookie_store, key: '_rebound_session'
